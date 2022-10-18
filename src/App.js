3b7a7dff61479c1bd09ebedfe9965ad015bf635b
Greg Kolb <greg.kolb@dolby.com>
2022-10-18 10:25:02 -0600

    fix subscription link to server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import {useSubscription, gql, useQuery } from '@apollo/client';
-
+import { useSubscription, gql, useQuery } from "@apollo/client";
 
 const TIME_SUBSCRIPTION = gql`
   subscription {
@@ -14,31 +13,29 @@ const HELLO_WORLD_QUERY = gql`
   query {
     placeholder
   }
-`
+`;
 
 const LatestTime = () => {
-  const { data, loading } = useSubscription(
-    TIME_SUBSCRIPTION
-  );
-  if (loading) return <h2>Loading...</h2>
+  const { data, loading, error } = useSubscription(TIME_SUBSCRIPTION);
+  if (error) {
+    console.log("SUBSCRIPTION ERROR: ", error);
+  }
+  if (loading) return <h2>Loading...</h2>;
   return <h4>New time obj: {JSON.stringify(data)}</h4>;
-}
+};
 
 const HelloWorld = () => {
-  const { data, loading } = useQuery(
-    HELLO_WORLD_QUERY
-  );
-  if (loading) return <h2>Loading...</h2>
+  const { data, loading } = useQuery(HELLO_WORLD_QUERY);
+  if (loading) return <h2>Loading...</h2>;
   return <h4>Hello World Query: {JSON.stringify(data)}</h4>;
-}
-
+};
 
 export default function App() {
   return (
     <div>
       <h2>My first Apollo app 🚀</h2>
-      <HelloWorld/>
-      <LatestTime/>
+      <HelloWorld />
+      <LatestTime />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,51 +1,48 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, split } from '@apollo/client';
-import App from './App';
-//checkout
-import { getMainDefinition } from '@apollo/client/utilities';
-//new ws protocol
-//import { createClient } from 'graphql-ws'
-//old ws protocol
-import { SubscriptionClient } from 'subscriptions-transport-ws'
-import { WebSocketLink } from '@apollo/client/link/ws'
-
-
-// const wsLink = new WebSocketLink({
-//   uri: `ws://localhost:8080/graphql`,
-//   options: {
-//     reconnect: true,
-//   },
-// });
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  split,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import App from "./App";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import { WebSocketLink } from "@apollo/client/link/ws";
 
 const wsLink = new WebSocketLink(
-  new SubscriptionClient("ws://192.168.0.12:8080/query")
+  new SubscriptionClient("ws://localhost:4000/query", {
+    reconnect: true,
+  })
 );
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:8080/query',
+  uri: "http://localhost:4000/query",
 });
 
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
-  httpLink,
+  httpLink
 );
 
 const client = new ApolloClient({
+  link: splitLink,
+  uri: "http://localhost:8080/query",
   cache: new InMemoryCache(),
-  link: splitLink
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>,
+  </ApolloProvider>
 );
